73e80457d6e398424c6739c2224c7cf8c0e70a0e
Connor Hainje <cmhainje@gmail.com>
2025-11-03 16:34:43 -0500

    update ingest to use new path for batchwriter and add force option

--- scripts/ingest.py
+++ scripts/ingest.py
@@ -13,8 +13,8 @@ from glob import glob
 from tqdm import tqdm
 from tqdm.contrib.logging import logging_redirect_tqdm
 
-from talltable.models import BatchWriter
-# from talltable.parallel_batch import ParallelBatchWriter
+from talltable.batch import BatchWriter
+
 from talltable.query import get_image_filepaths
 from talltable.paths import DATA_DIR
 
@@ -26,27 +26,25 @@ def parse():
     ap = ArgumentParser()
     ap.add_argument("mjd", nargs="?", default=None)
     ap.add_argument("-N", "--num-workers", type=int, nargs="?", default=8)
-    ap.add_argument("-C", "--chunk-size",  type=int, nargs="?", default=16)
+    ap.add_argument("-C", "--chunk-size", type=int, nargs="?", default=16)
+    ap.add_argument("-f", "--force", action="store_true", help="force (re-ingest)")
     return ap.parse_args()
 
 
 def main(args):
-    data_files = glob(str(DATA_DIR / (("*" if args.mjd is None else f"{args.mjd}") + "/*.fits")))
-    to_ingest = set(data_files) - set(get_image_filepaths())
+    data_files = glob(
+        str(DATA_DIR / (("*" if args.mjd is None else f"{args.mjd}") + "/*.fits"))
+    )
+    done_before = set(get_image_filepaths()) if not args.force else set()
+    to_ingest = set(data_files) - done_before
     to_ingest = sorted(list(to_ingest))
     logger.info("%d files to ingest", len(to_ingest))
-
-    # batch = ParallelBatchWriter(num_workers=args.num_workers)
-    # for chunk_start in tqdm(range(0, len(to_ingest), args.chunk_size), unit="chunk"):
-    #     filepaths = to_ingest[chunk_start : chunk_start + args.chunk_size]
-    #     logger.info(f"chunk {chunk_start // args.chunk_size}:\n  " + "\n  ".join(filepaths))
-    #     batch.process_batch(filepaths)
 
     batch = BatchWriter(chunk_size=args.chunk_size)
 
     for index in tqdm(range(len(to_ingest))):
         filepath = to_ingest[index]
-        logger.info("processing %s", filepath)
+        logger.info("processing %s", str(filepath).replace(str(DATA_DIR) + '/', ''))
         batch.process_image(filepath)
 
     # if we finish with an unwritten partial chunk, write it out
