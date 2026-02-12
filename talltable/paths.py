a96dbe70d115d6e8c931b5071c8146a06d4bc99e
Connor Hainje <cmhainje@gmail.com>
2026-02-12 12:24:15 -0800

    update paths to popeye

--- talltable/paths.py
+++ talltable/paths.py
@@ -3,9 +3,9 @@ from pathlib import Path
 SRC_DIR  = Path(__file__).parent.absolute()
 PROJ_DIR = SRC_DIR.parent.absolute()
 
-DATA_DIR = Path("/Volumes/databoy/spherex")
+DATA_DIR = Path("/mnt/sdceph/users/spherex/spherex_data_qr2")
 
-DB_DIR   = (PROJ_DIR / "db").absolute()
+DB_DIR   = Path("/mnt/sdceph/users/chainje/spxdb")
 PIXEL_DB_PATH = DB_DIR / "pixels"
 IMAGE_DB_PATH = DB_DIR / "image.parquet"
 WAVES_DB_PATH = DB_DIR / "waves.parquet"
