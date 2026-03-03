55f22a60f20b3773841e718f982b7ad8963483cb
Connor Hainje <cmhainje@gmail.com>
2026-03-02 20:20:51 -0500

    update partitioning once again

--- scripts/build_wave.py
+++ scripts/build_wave.py
@@ -8,7 +8,7 @@ from tqdm import tqdm
 
 from talltable.paths import DATA_DIR, WAVES_DB_PATH
 from talltable.waveid import rowcoldet_to_waveid
-from talltable.constants import N_WAVEPART, ALL_ROW, ALL_COL
+from talltable.constants import ALL_ROW, ALL_COL
 from talltable.util import byteswap
 
 
@@ -21,7 +21,6 @@ data = {
     "waveid": [],
     "wavelength": [],
     "bandwidth": [],
-    "wavepart": [],
 }
 
 waveids_nodet = rowcoldet_to_waveid(ALL_ROW, ALL_COL, 0)
@@ -33,25 +32,6 @@ for filepath in tqdm(spec_files):
         data["bandwidth"].append(byteswap(f["CBAND"].data[ALL_ROW, ALL_COL]))
 
 for k, v in data.items():
-    if k != "wavepart":
-        data[k] = np.concatenate(v).squeeze()
-
-
-def partition_edges(wavelengths, n_part):
-    wavesrt = np.sort(wavelengths)
-    stride = len(wavesrt) // n_part
-
-    bounds = np.arange(1, n_part) * stride
-    edges = np.zeros(n_part + 1)
-    edges[1:-1] = 0.5 * (wavesrt[bounds - 1] + wavesrt[bounds])
-    edges[0] = wavesrt[0] - 0.5 * (wavesrt[1] - wavesrt[0])
-    edges[-1] = wavesrt[-1] + 0.5 * (wavesrt[-1] - wavesrt[-2])
-    return edges
-
-
-edges = partition_edges(data["wavelength"], N_WAVEPART)
-data["wavepart"] = np.digitize(data["wavelength"], edges) - 1
-assert np.count_nonzero(data["wavepart"] < 0) == 0
-assert np.count_nonzero(data["wavepart"] >= N_WAVEPART) == 0
+    data[k] = np.concatenate(v).squeeze()
 
 pq.write_table(pa.table(data), WAVES_DB_PATH)
--- talltable/paths.py
+++ talltable/paths.py
@@ -1,17 +1,17 @@
 from pathlib import Path
 
-SRC_DIR  = Path(__file__).parent.absolute()
+SRC_DIR = Path(__file__).parent.absolute()
 PROJ_DIR = SRC_DIR.parent.absolute()
 
 DATA_DIR = Path("/mnt/sdceph/users/spherex/spherex_data_qr2")
+DB_DIR = Path("/mnt/sdceph/users/chainje/spxdb")
 
-DB_DIR   = Path("/mnt/sdceph/users/chainje/spxdb")
 PIXEL_DB_PATH = DB_DIR / "pixels"
 IMAGE_DB_PATH = DB_DIR / "image.parquet"
 WAVES_DB_PATH = DB_DIR / "waves.parquet"
 EPHEM_DB_PATH = DB_DIR / "ephem.parquet"
-
 IMAGE_PARTS_DIR = DB_DIR / "image_parts"
+
 
 def image_part_path(task_id: int) -> Path:
     return IMAGE_PARTS_DIR / f"image_task{task_id}.parquet"
