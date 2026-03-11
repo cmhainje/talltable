e59395f0d7afc3dfcb45d089c2d6cfb1029011ee
Connor Hainje <cmhainje@gmail.com>
2026-03-11 12:29:06 -0400

    overhaul partitioning scheme

--- src/talltable/constants.py
+++ src/talltable/constants.py
@@ -1,13 +1,19 @@
 import numpy as np
-from astropy.coordinates import SkyCoord
 
-HP_PART_LEVEL = 6
-DF_PART_LEVEL = 9
+PART_MIN_LEVEL = 6
+PART_MAX_LEVEL = 10
 HP_HIGH_LEVEL = 22
-DF_SAFE_RADIUS = 6
 
 _idx = np.arange(2040, dtype=np.uint32)
 ALL_ROW, ALL_COL = map(np.ravel, np.meshgrid(_idx, _idx, indexing="ij"))
 
-NORTH_DEEP_FIELD = SkyCoord(lat="90d", lon="0d", frame="geocentricmeanecliptic")
-SOUTH_DEEP_FIELD = SkyCoord(lat="-82d", lon="+44.8d", frame="geocentricmeanecliptic")
+MAX_ROWS_PER_PART = 200_000_000
+
+
+# HP_PART_LEVEL = 6
+# DF_PART_LEVEL = 9
+# DF_SAFE_RADIUS = 6
+# 
+# from astropy.coordinates import SkyCoord
+# NORTH_DEEP_FIELD = SkyCoord(lat="90d", lon="0d", frame="geocentricmeanecliptic")
+# SOUTH_DEEP_FIELD = SkyCoord(lat="-82d", lon="+44.8d", frame="geocentricmeanecliptic")
