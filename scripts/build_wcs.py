36ef788d2473a8d19df81a42d4bfebd39a6594a1
Connor Hainje <cmhainje@gmail.com>
2026-05-20 09:43:04 -0400

    new script for building WCS table

---
+++ scripts/build_wcs.py
@@ -0,0 +1,103 @@
+import duckdb
+import pyarrow as pa
+import pyarrow.parquet as pq
+
+from astropy.io import fits
+from os.path import basename
+from tqdm.auto import tqdm
+
+from talltable.paths import DATA_DIR, IMAGE_DB_PATH, WCS_DB_PATH
+
+
+# *** FIGURE OUT WHICH TO INGEST ***
+
+# collect all ingested files
+ingested = duckdb.sql(f"SELECT imageid, filepath FROM read_parquet('{IMAGE_DB_PATH}')").fetchall()
+#      ^ list of (id, path) tuples
+
+# collect the known ids
+if WCS_DB_PATH.exists():
+    _result = duckdb.sql(f"SELECT imageid FROM read_parquet('{WCS_DB_PATH}')").fetchall()
+    known_ids = set(x[0] for x in _result)
+else:
+    known_ids = set()
+
+# collect the known filepaths (remove this in prod)
+local_files = set(p.name for p in DATA_DIR.glob("*.fits"))
+
+# make a list of all the new imageids and filepaths to do
+todo = []
+for (_id, _path) in ingested:
+    _name = basename(_path)
+    if _id not in known_ids and _name in local_files:
+        todo.append((_id, DATA_DIR / _name))
+
+del ingested, known_ids
+
+print(f"{len(todo)} files identified")
+
+
+# *** collect the WCS params of those new images ***
+
+# set up the data dict
+new_data = {
+    "imageid": [],
+    "CRPIX1": [],
+    "CRPIX2": [],
+    "CRVAL1": [],
+    "CRVAL2": [],
+    "PC1_1": [],
+    "PC1_2": [],
+    "PC2_1": [],
+    "PC2_2": [],
+}
+
+for coeff in ['A', 'B']:
+    for i in range(4):
+        for j in range(4 - i):
+            col = f"{coeff}_{i}_{j}"
+            new_data[col] = []
+
+# loop over the images
+for (_id, _path) in tqdm(todo):
+    try:
+        with fits.open(_path) as hdul:
+            new_data['imageid'].append(_id)
+            header = hdul["IMAGE"].header
+
+            for key in new_data.keys():
+                if key == 'imageid':
+                    continue
+                new_data[key].append(header.get(key, 0.0))
+    except FileNotFoundError:
+        print(f"warning: {_path} not found")
+
+# cast to PyArrow table with columns sorted alphabetically
+new_table = pa.table({k: new_data[k] for k in sorted(new_data)})
+
+
+# *** write it out ***
+
+if WCS_DB_PATH.exists():
+    old_table = pq.ParquetFile(WCS_DB_PATH).read()
+    table = pa.concat_tables(old_table, new_table)
+    tmp_path = WCS_DB_PATH.with_suffix('.tmp')
+    pq.write_table(
+        table,
+        tmp_path,
+        compression="zstd",
+        compression_level=3,
+        use_dictionary=False,
+    )
+    tmp_path.replace(WCS_DB_PATH)
+
+else:
+    pq.write_table(
+        new_table,
+        WCS_DB_PATH,
+        compression="zstd",
+        compression_level=3,
+        use_dictionary=False,
+    )
+
+
